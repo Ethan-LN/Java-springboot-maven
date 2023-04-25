7bac69ac774c1cc6e53d4b314ed90e90162d432f
GIT_AUTHOR_NAME <GIT_AUTHOR_EMAIL>
2023-04-25 16:38:08 +1000

    setup the springboot

---
+++ src/test/java/springbootmaven/springboot/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootmaven.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
