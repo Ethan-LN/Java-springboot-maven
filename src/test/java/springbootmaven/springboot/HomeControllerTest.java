59fb113f00fc28182283e119b9ed4a9eb82c8fa5
GIT_AUTHOR_NAME <GIT_AUTHOR_EMAIL>
2023-04-29 23:18:31 +1000

    fix the html to past the test

--- src/test/java/springbootmaven/springboot/HomeControllerTest.java
+++ src/test/java/springbootmaven/springboot/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package springbootmaven.springboot;
 
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -10,10 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-@WebMvcTest(HomeController.class)
+@WebMvcTest(springbootmaven.springboot.HomeController.class)
 public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
     @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
@@ -21,3 +21,5 @@ public class HomeControllerTest {
                 .andExpect(view().name("home"))
                 .andExpect(content().string(
                         containsString("Welcome to...")));
+    }
+}
