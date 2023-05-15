3c6a7d3f4e9bec1730a2ba1dca0006904b0e44cf
GIT_AUTHOR_NAME <GIT_AUTHOR_EMAIL>
2023-05-15 17:45:10 +1000

    adjust the directories of javaclasses

--- src/main/java/springbootmaven/springboot/controllers/HomeController.java
+++ src/main/java/springbootmaven/springboot/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package springbootmaven.springboot;
+package springbootmaven.springboot.components;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
