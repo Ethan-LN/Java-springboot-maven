a75651f09a9f931a44dd228ded69b4955f14c783
GIT_AUTHOR_NAME <GIT_AUTHOR_EMAIL>
2023-05-17 21:07:50 +1000

    temperate saving need to fix the problem with compatibility issues with javax, jarkata, springboot 2.7.11 and java 11

--- src/main/java/springbootmaven/springboot/components/Taco.java
+++ src/main/java/springbootmaven/springboot/components/Taco.java
@@ -2,8 +2,17 @@ package springbootmaven.springboot.components;
 
 import java.util.List;
 import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation
+import lombok.NonNull;
+
 @Data
 public class Taco {
+    @NonNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+    @NonNull
+    @Size()
     private List<Ingredient> ingredients;
 }
